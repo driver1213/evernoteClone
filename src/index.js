45ee4f3ac78451ee19f1700025290740f590bbdc
diego rivera <diegora1377@gmail.com>
2020-04-12 00:08:08 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -6,15 +6,7 @@ import App from './App';
 const firebase = require('firebase')
 require('firebase/firestore')
 
-firebase.initializeApp({
-    apiKey: "AIzaSyAkRU4lg_BSJNwfOmrPanmEWYzB0DEYcnQ",
-    authDomain: "evernote-clone-35afb.firebaseapp.com",
-    databaseURL: "https://evernote-clone-35afb.firebaseio.com",
-    projectId: "evernote-clone-35afb",
-    storageBucket: "evernote-clone-35afb.appspot.com",
-    messagingSenderId: "935707292463",
-    appId: "1:935707292463:web:a69cc85525e94ff4f067da",
-})
+
 
 ReactDOM.render(
     <App />
